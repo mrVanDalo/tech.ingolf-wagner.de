86d75a91ba5011849c6bdeb4fb770de98872ca62
Ingolf Wagner <contact@ingolf-wagner.de>
2019-12-20 20:00:22 +1300

    run nixfmt

--- shell.nix
+++ shell.nix
@@ -1,16 +1,14 @@
 { pkgs ? import <nixpkgs> { } }:
 let
-  updateCabal = pkgs.writers.writeBashBin "update-cabal"
-    ''
-      echo "# created by cabal2nix " > ${toString ./.}/current-project.nix
-      ${pkgs.cabal2nix}/bin/cabal2nix ${toString ./.} >> ${
-        toString ./.
-      }/current-project.nix
-    '';
+  updateCabal = pkgs.writers.writeBashBin "update-cabal" ''
+    echo "# created by cabal2nix " > ${toString ./.}/current-project.nix
+    ${pkgs.cabal2nix}/bin/cabal2nix ${toString ./.} >> ${
+      toString ./.
+    }/current-project.nix
+  '';
 
-    run = pkgs.writers.writeBashBin "run"
-    ''
-      ${pkgs.cabal-install}/bin/cabal run site -- watch
-      '';
+  run = pkgs.writers.writeBashBin "run" ''
+    ${pkgs.cabal-install}/bin/cabal run site -- watch
+  '';
 in pkgs.mkShell { buildInputs = with pkgs; [ updateCabal run ]; }
 
