c7852f1c08ef038c5c334af195115239dea194df
Ingolf Wagner <contact@ingolf-wagner.de>
2019-12-21 13:05:20 +1300

    use old css and templates

---
+++ index.markdown
@@ -0,0 +1,12 @@
+---
+title: Tech Articles
+---
+
+# Tech articles
+
+Hey here are some articles about technical issues. Most of them are about [NixOS](https://nixos.org)
+topics, because it is the OS of my choice.
+
+If you are interessted in more output of me please visit “[my other page](https://ingolf-wagner.de)”.
+
+$partial("templates/post-list.html")$
--- site.hs
+++ site.hs
@@ -9,6 +9,7 @@ import Text.Pandoc.Options
 main :: IO ()
 main =
   hakyll $ do
+    -- copy all images
     match "images/*" $ do
       route idRoute
       compile copyFileCompiler
@@ -20,51 +21,24 @@ main =
         withItemBody
           (unixFilter "lessc" ["-", "--include-path=./src/lessc/page/"]) >>=
         return . fmap compressCss
-    match "css/*" $ do
-      route idRoute
-      compile compressCssCompiler
-    match (fromList ["about.rst", "contact.markdown"]) $ do
-      route $ setExtension "html"
-      compile $
-        pandocCompiler >>=
-        loadAndApplyTemplate "templates/default.html" defaultContext >>=
-        relativizeUrls
     -- the new content out there
     match "new-content/**" $ do
       route $
         gsubRoute "new-content/" (const "") `composeRoutes` setExtension "html"
       compile $
         pandocCompilerWithToc >>=
-        loadAndApplyTemplate "templates/new.html" postCtx >>=
+        loadAndApplyTemplate "templates/layout.html" postCtx >>=
         relativizeUrls
-    match "posts/*" $ do
+    match "index.markdown" $ do
       route $ setExtension "html"
-      compile $
-        pandocCompiler >>= loadAndApplyTemplate "templates/post.html" postCtx >>=
-        loadAndApplyTemplate "templates/default.html" postCtx >>=
-        relativizeUrls
-    create ["archive.html"] $ do
-      route idRoute
       compile $ do
-        posts <- recentFirst =<< loadAll "posts/*"
-        let archiveCtx =
-              listField "posts" postCtx (return posts) `mappend`
-              constField "title" "Archives" `mappend`
-              defaultContext
-        makeItem "" >>= loadAndApplyTemplate "templates/archive.html" archiveCtx >>=
-          loadAndApplyTemplate "templates/default.html" archiveCtx >>=
-          relativizeUrls
-    match "index.html" $ do
-      route idRoute
-      compile $ do
-        posts <- loadAll "posts/*"
         newContent <- loadAll "new-content/**"
         let indexCtx =
-              listField "posts" postCtx (recentFirst $ newContent ++ posts) `mappend`
+              listField "posts" postCtx (recentFirst $ newContent ) `mappend`
               constField "title" "Home" `mappend`
               defaultContext
-        getResourceBody >>= applyAsTemplate indexCtx >>=
-          loadAndApplyTemplate "templates/default.html" indexCtx >>=
+        pandocCompilerWithoutToc >>= applyAsTemplate indexCtx >>=
+          loadAndApplyTemplate "templates/layout.html" indexCtx >>=
           relativizeUrls
     match "templates/*" $ compile templateBodyCompiler
 
